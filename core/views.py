df29eb2f264c5a65ebe1304a2190e88ff8ca7d9a
Lucas Oliveira <lucax.oliveira96@gmail.com>
2022-01-11 15:22:15 -0300

    Incluido função e tela de autenticação.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 from core.models import Evento
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 def eventos(request, titulo_evento):
@@ -10,9 +12,29 @@ def eventos(request, titulo_evento):
     except:
         return HttpResponse('Não encontrado')
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválido.")
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    #usuario = request.user
-    #evento = Evento.objects.filter(usuario=usuario)
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
+    #evento = Evento.objects.all()
     response = {'evento':evento}
     return render(request, 'agenda.html', response)
